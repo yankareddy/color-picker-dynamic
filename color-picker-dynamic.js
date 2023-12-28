f759d1bbb7542aca6648bfb0d9334ec3908bcd23
yankareddy <reddy.a111friend@gmail.com>
2023-12-28 19:31:34 +0530

    color-picker-dynamic

---
+++ color-picker-dynamic.js
@@ -0,0 +1,23 @@
+function colorPick1() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").style.color = "#49a6e9";
+}
+
+function colorPick2() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+    document.getElementById("selectedColorHexCode").style.color = "#49a6e9";
+}
+
+function colorPick3() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+    document.getElementById("selectedColorHexCode").style.color = "#49a6e9";
+}
+
+function colorPick4() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").style.color = "#49a6e9";
+}
